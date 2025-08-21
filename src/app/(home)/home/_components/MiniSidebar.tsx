7aaa9a5b636bcf9babf7bfff08960f54aea1fd9d
Uday vara <varaudayd@gmail.com>
2025-08-22 00:27:18 +0530

    added foundation layout

---
+++ src/app/(home)/home/_components/MiniSidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MiniSidebar() {
+  return (
+    <div className="h-full text-neutral-200  overflow-hidden bg-[#4a154b] w-[68px] max-h-full overflow-y-auto max-w-[68px] border-r border-r-[#5e285f] shadow">
+        <div className="flex flex-col h-full w-full items-center justify-center text-wrap">
+            <h4 className='text-wrap'>MiniSidebar</h4>
+        </div>
+    </div>
+  )
+}
+
+export default MiniSidebar
--- src/app/(home)/home/page.tsx
+++ src/app/(home)/home/page.tsx
@@ -1,5 +1,7 @@
 import { auth } from "@/auth/auth"
 import { Metadata } from "next";
+import MiniSidebar from "./_components/MiniSidebar";
+import ChatSidebar from "./_components/ChatSidebar";
 
 export const metadata: Metadata = {
     title: "Home | Slack",
@@ -9,8 +11,12 @@ export default async function Home () {
     const user = await auth()
     console.log("user",user)
     return (
-        <div className="flex flex-col w-full h-full items-center justify-center items-center text-2xl">
+        <div className="h-full w-full gap-0 flex flex-row">
+            <MiniSidebar />
+            <ChatSidebar />
+        <div className="flex flex-col w-full h-full items-center justify-center text-2xl">
             {user ? <h4>Hello {user?.user?.email}</h4> : <h4>User Not Logged In</h4>}
         </div>
+       </div>
     )
 }
