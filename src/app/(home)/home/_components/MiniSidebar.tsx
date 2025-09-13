6447efc096f7aacfff79c786aac6454a2f2d5dc9
Uday vara <varaudayd@gmail.com>
2025-09-13 18:33:53 +0530

    refactor mini sidebar styles

--- src/app/(home)/home/_components/MiniSidebar.tsx
+++ src/app/(home)/home/_components/MiniSidebar.tsx
@@ -7,25 +7,28 @@ import { TbMessageCircle } from 'react-icons/tb'
 
 function MiniSidebar() {
   return (
-    <div className="h-full text-neutral-200  overflow-hidden bg-primary-theme w-[68px] max-h-full overflow-y-auto max-w-[68px] border-r border-r-[#5e285f] flex flex-col gap-5 shadow pt-1">
-          <Link href={"/home"} className="icon-wrap flex flex-col items-center justify-center gap-0.5  transtion-all duration-200 ease-in-out cursor-pointer group rounded">
-            <AiFillHome className='text-4xl  py-1 px-2 hover:bg-[#926093] rounded' />
-            <span className="text-white text-xs">Home</span>
-          </Link>
-          
-          <Link href={"/direct-messages"} className="icon-wrap flex flex-col items-center justify-center gap-0.5  transtion-all duration-200 ease-in-out cursor-pointer group rounded">
-            <TbMessageCircle fill='#ffffff' className='text-4xl   py-1 px-2 hover:bg-[#926093] rounded' />
-            <span className="text-white  text-xs">DMs</span>
-          </Link>
-          <Link href={"activity"} className="icon-wrap flex flex-col items-center justify-center gap-0.5  transtion-all duration-200 ease-in-out cursor-pointer group rounded">
-            <IoNotifications className='text-4xl  py-1 px-2 hover:bg-[#926093] rounded' />
-            <span className="text-white text-xs">Activity</span>
-          </Link>
-          <Link href={"/files"} className="icon-wrap flex flex-col items-center justify-center gap-0.5  transtion-all duration-200 ease-in-out cursor-pointer group rounded">
-            <LuFiles  className='text-4xl  py-1 px-2 hover:bg-[#926093] rounded' />
-            <span className="text-white text-xs">Files</span>
-          </Link>
-         
+    <div className="h-full text-neutral-200 bg-primary-theme w-[68px] max-h-full overflow-y-auto border-r border-r-[#5e285f] flex flex-col gap-5 shadow pt-1">
+      
+      <Link href="/home" className="flex flex-col items-center justify-center gap-0.5 cursor-pointer group rounded">
+        <AiFillHome className="text-4xl py-1 px-2 rounded transition-transform duration-200 transform group-hover:scale-125 group-hover:bg-[#926093]" />
+        <span className="text-white text-xs">Home</span>
+      </Link>
+
+      <Link href="/direct-messages" className="flex flex-col items-center justify-center gap-0.5 cursor-pointer group rounded">
+        <TbMessageCircle className="text-4xl py-1 px-2 rounded transition-transform duration-200  group-hover:bg-[#926093]" />
+        <span className="text-white text-xs">DMs</span>
+      </Link>
+
+      <Link href="/activity" className="flex flex-col items-center justify-center gap-0.5 cursor-pointer group s rounded">
+        <IoNotifications className="text-4xl py-1 px-2 rounded transition-transform duration-200   group-hover:bg-[#926093]" />
+        <span className="text-white text-xs">Activity</span>
+      </Link>
+
+      <Link href="/files" className="flex flex-col items-center justify-center gap-0.5 cursor-pointer group rounded">
+        <LuFiles className="text-4xl py-1 px-2 rounded transition-transform duration-200 group-hover:bg-[#926093]" />
+        <span className="text-white text-xs">Files</span>
+      </Link>
+      
     </div>
   )
 }
